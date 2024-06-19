22265976da04df59b05eb54bc4ef6d5aee429417
David Aslan <davidaslan112671@live.se>
2024-06-19 15:08:39 +0200

    image uploads

--- src/app/api/Identify/route.ts
+++ src/app/api/Identify/route.ts
@@ -1,33 +1,21 @@
 import { UploadedImage } from "@/app/page";
 import { prisma } from "@/lib/dbService";
 import { Prisma, User } from "@prisma/client";
-
+import { put } from "@vercel/blob";
+import { NextResponse } from "next/server";
 interface RequestData {
-  images: UploadedImage[];
+  images: File[];
   description: string;
   user: User;
 }
 
 export async function POST(request: Request) {
   try {
-    const formData = await request.formData();
-
-    console.log()
-
-    const description: string = formData.get("description") as string;
-    const user: User = JSON.parse(formData.get("user") as string);
-    const uploadedImages: File[] = formData.getAll("images") as File[];
-
-    // Process the received data as needed
-    console.log(description);
-    console.log(user);
-    console.log(uploadedImages);
-
-    console.log("USER ID: ", user.id);
-
+    const parsedRequestData = await ParseFormData(request);
+    const uploadedImageUrls = await UploadImages(parsedRequestData);
     const updatedUser = await prisma.user.update({
       where: {
-        id: user.id,
+        id: parsedRequestData.user.id,
       },
       data: {
         aiTokens: {
@@ -45,4 +33,44 @@ export async function POST(request: Request) {
     console.error("Error processing request:", error);
     return Response.json({ error: "Internal Server Error" });
   }
+}
+
+async function ParseFormData(request: Request) {
+  const formData = await request.formData();
+  const description: string = formData.get("description") as string;
+  const user: User = JSON.parse(formData.get("user") as string);
+  const uploadedImages: File[] = formData.getAll("images") as File[];
+  console.log(description);
+  console.log(user);
+  console.log(uploadedImages);
+  console.log("USER ID: ", user.id);
+
+  const parsedRequestData: RequestData = {
+    description: description,
+    user: user,
+    images: uploadedImages,
+  };
+
+  return parsedRequestData;
+}
+
+async function UploadImages(parsedRequestData: RequestData) {
+  // Array to store URLs of uploaded images
+  const uploadedImageUrls: string[] = [];
+
+  // Upload each image
+  for (const image of parsedRequestData.images) {
+    const arrayBuffer = await image.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    // Upload image to Vercel Blob storage
+    const { url } = await put(
+      `user-uploads/${parsedRequestData.user.id}/${image.name}`,
+      buffer,
+      { access: "public" }
+    );
+    uploadedImageUrls.push(url);
+  }
+  console.log("Uploaded image URLs:", uploadedImageUrls);
+  return uploadedImageUrls;
 }
